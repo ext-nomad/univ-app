44f1a1455858bf36e39ee1a50a52cf365f8e5da9
ext-nomad <i.dwanderer@gmail.com>
2020-10-22 21:33:37 +0300

    add/ session controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true,
-                   uniqueness: { case_sensitive: false },
                    length: { minimum: 3, maximum: 25 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
   validates :email, presence: true,
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'about', to: 'home#about'
+
+  get 'signup', to: 'users#new'
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
   resources :courses
-  resources :students, except: %i[destroy]
+  resources :users, except: %i[new destroy]
 end
