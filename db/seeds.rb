cd653c578b3b84021aca25221b75544874e58a67
ext-nomad <i.dwanderer@gmail.com>
2020-10-24 22:50:01 +0300

    update/ faker seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,21 @@
 10.times do |n|
   name = Faker::Name.first_name
   email = "example-#{n + 1}@example.com"
-  # password = 'password'
   User.create!(name: name, email: email, password: email)
 end
 
-30.times do
-  # rand_id = Faker::Number.between(from: 2, to: 6)
+40.times do
   Course.create!([{
                    short_name: Faker::Educator.unique.course_name,
                    name: Faker::Educator.subject,
-                   description: Faker::Lorem.paragraph(sentence_count: 5)
+                   description: Faker::Lorem.paragraph(sentence_count: 10)
                  }])
+end
+
+def rand_id(from, to)
+  Faker::Number.between(from: from, to: to)
+end
+
+30.times do
+  UserCourse.create!([{ user_id: rand_id(1, 10), course_id: rand_id(1, 40) }])
 end
